a644e733e97ac31c4bf83a04fe7e69577a4753c4
Shaun <thedeadsent@gmail.com>
2021-11-22 20:33:56 -0500

    more tables

--- db/init_db.js
+++ db/init_db.js
@@ -12,8 +12,10 @@ const {
 async function buildTables() {
   try {
     client.connect();
-    console.log("Dropping tables!")
+    console.log("Dropping tables!");
     await client.query(`
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS cart;
     DROP TABLE IF EXISTS reviews;
     DROP TABLE IF EXISTS product_tags;
     DROP TABLE IF EXISTS tags;
@@ -21,9 +23,9 @@ async function buildTables() {
     DROP TABLE IF EXISTS users;
     `);
     // drop tables in correct order
-console.log("Finished dropped tables!")
+    console.log("Finished dropped tables!");
 
-console.log("Building tables.")
+    console.log("Building tables.");
     // build tables in correct order
     await client.query(`
       CREATE TABLE users(
@@ -57,11 +59,20 @@ console.log("Building tables.")
         title VARCHAR(255) NOT NULL,
         content VARCHAR(255) NOT NULL
       );
-    
+    CREATE TABLE cart (
+      id SERIAL PRIMARY KEY,
+      "cartOwner" VARCHAR(255) REFERENCES users(username),
+      "itemId" INTEGER REFERENCES products(id)
+    );
+    CREATE TABLE orders (
+      id SERIAL PRIMARY KEY,
+      "orderStatus" BOOLEAN DEFAULT 'false',
+      owner VARCHAR(255) REFERENCES users(username),
+      "cartId" INTEGER REFERENCES cart(id)
+    );
     `);
 
-    console.log("Finished building tables!")
-
+    console.log("Finished building tables!");
   } catch (error) {
     throw error;
   }
