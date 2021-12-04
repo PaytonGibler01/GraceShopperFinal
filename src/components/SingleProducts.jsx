d1362c611993b118fe656d729e0fdeb6604800bf
Shaun <thedeadsent@gmail.com>
2021-12-04 08:38:52 -0500

    single product page still not working

--- src/components/SingleProducts.jsx
+++ src/components/SingleProducts.jsx
@@ -1,3 +1,18 @@
 // import React from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
+const SingleProducts = ({product}) => {
+    return (
+        <div key={product.id} className="product-card">
+          <img src={product.image}></img>
+          <h4>{product.name}</h4>
+          <p>Price: {product.price}</p>
+          </div>
+          )
+}
+
+
+
+export default SingleProducts
 // export default SingleProducts;
--- src/components/SingleProductsPage.jsx
+++ src/components/SingleProductsPage.jsx
@@ -1,26 +1,27 @@
-// import React from "react";
-// import { useParams } from "react-router";
-// import { SingleProducts } from ".";
+import React from "react";
+import { useParams } from "react-router";
+import { SingleProducts } from ".";
+import { getProductById } from "../api/products";
 
+const SingleProductsPage = ({ products }) => {
+  const { productId } = useParams();
+  console.log(productId, "Product ID Single Products Page")
+    console.log(products, "Single Products Page Log")
+  const compProduct = products.find((product) => product.id === productId)
+    console.log(compProduct, "compProduct Log")
+  if (!compProduct) {
+    return (
+      <div className="product-card">
+        <h3> The product with id {productId} was not found</h3>
+      </div>
+    );
+  }
 
-// const SingleProductsPage = ({ allProducts }) => {
-//   const { productId } = useParams();
+  return (
+    <div className="products-main-container">
+      <SingleProducts product={compProduct} />
+    </div>
+  );
+};
 
-//   const compProduct = allProducts.find((product) => product._id === productId)
-
-//   if (!compProduct) {
-//     return (
-//       <div className="product-card">
-//         <h3> The product with id {productId} was not found</h3>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="products-main-container">
-//       <SingleProducts product={compProduct} />
-//     </div>
-//   );
-// };
-
-// export default SingleProductsPage;
+export default SingleProductsPage;
