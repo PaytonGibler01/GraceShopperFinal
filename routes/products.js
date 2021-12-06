a4b72a18919aa0698aded8ed483473bf61cc95b5
HannahWroblewski <hannah.wrob@gmail.com>
2021-12-05 19:52:28 -0500

    newProductForm.jsx

--- routes/products.js
+++ routes/products.js
@@ -64,4 +64,14 @@ productsRouter.get("/tags", async (req, res, next) => {
   next();
 });
 
+// productsRouter.post('/api/images', parser.single("image"), (req, res) => {
+//   console.log(req.file) // to see what is returned to you
+//   const image = {};
+//   image.url = req.file.url;
+//   image.id = req.file.public_id;
+//   Image.create(image) // save image information in database
+//     .then(newImage => res.json(newImage))
+//     .catch(err => console.log(err));
+// });
+
 module.exports = productsRouter;
---
+++ src/components/NewProductForm.jsx
@@ -0,0 +1,80 @@
+import React, {useState} from "react";
+import {getProducts} from "../api/products";
+import {getToken, getUser} from "../auth";
+
+const NewProductForm = ({setAllProducts, allProducts}) => {
+const [title, setTitle] = useState('')
+const [description, setDescription] = useState('')
+const [price, setPrice] = useState('')
+
+    return (
+        <div className="new-product-component-main-container">
+            <form
+                id="newProductSubmit"
+                onSubmit={async (event)=>{
+                    event.preventDefault();
+                    try {
+                        const token = getToken();
+                        const user = getUser();
+                        const createNewProduct = await getProducts(title, description, price, token);
+                        setAllPosts([createNewProduct, ...allProducts]);
+
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }}
+            >
+                <h2>Create Posts Here</h2>
+                <fieldset className="auth-component_input">
+                <label htmlFor="title">Title: </label>
+                <input
+                id="title"
+                type="text"
+                placeholder="enter title"
+                value={title}
+                onChange={(event)=> {
+                    setTitle(event.target.value);
+                }}
+                ></input>
+            </fieldset>
+
+            <form action='/api/images' method="post" enctype="multipart/form-data">
+            <input type='file' name='image' />
+            </form>
+
+            {/* use <img src=imageURL /> in a database query to display image on the front-end*/}
+
+            <fieldset className="auth-component_input">
+                <label htmlFor="description">Description: </label>
+                <input
+                id="description"
+                type="text"
+                placeholder="enter description"
+                value={description}
+                onChange={(event)=> {
+                    setDescription(event.target.value);
+                }}
+                ></input>
+            </fieldset>
+            <fieldset className="auth-component_input">
+                <label htmlFor="price">Price: </label>
+                <input
+                id="price"
+                type="text"
+                placeholder="Price"
+                value={price}
+                onChange={(event)=> {
+                    setPrice(event.target.value);
+                }}
+                ></input>
+            </fieldset>
+ 
+            <button type="submit" id="newProductSubmit"
+            >Submit Your Ship!</button>
+            </form>
+
+        </div>
+    );
+}
+
+export default NewProductForm;  
