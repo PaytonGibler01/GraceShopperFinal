55259977402a7c2df5c1a5ccd9ecaa0136bad3b8
HannahWroblewski <hannah.wrob@gmail.com>
2021-12-11 12:19:28 -0500

    really basic profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 const Profile = () => {
     return (
-      <>
-        <header>
-          <h1>Welcome to Profile Component</h1>
-        </header>
-      </>
+        <div className="profile-card">
+          <h1>My Purchases</h1>
+          <h3>You haven't purchased anything yet!</h3>
+          <h1>My Reviews</h1>
+          <h3>You haven't left any reviews yet!</h3>
+        </div>
     );
   };
   
